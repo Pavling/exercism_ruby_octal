23251b105627d9ae7286ed98173085bba7f23e33
Michael Pavling <pavling@gmail.com>
2013-11-06 09:33:58 +0000

    refactor for clarity

--- octal.rb
+++ octal.rb
@@ -4,9 +4,13 @@ class Octal
     end
 
     def to_decimal
-      @octal.to_s.reverse.chars.each_with_index.map do |char, index|
-        char == 0 ? 0 : (8**index) * char.to_i
-      end.reduce(:+)
+      @octal.to_s.reverse.chars.each_with_index.reduce(0) do |sum, (char, index)|
+        sum += decimal_value_of_octal_digit(char.to_i, index)
+      end
     end
 
+    private
+    def decimal_value_of_octal_digit(digit, column)
+      digit == 0 ? 0 : (8**column) * digit
+    end
 end
